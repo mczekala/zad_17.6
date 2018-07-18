9955cdc1ed119b5565ae95f853c6f7e495f66160
Marcin Czekala <marcin_czekala@icloud.com>
2018-07-18 14:40:38 +0200

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+app.get('/', function(req, res) {
+  res.send('Zalogowano');
+});
+app.get('/auth/google', function(req, res) {
+  res.render('first-template');
+});
+
+app.listen(3000);
+app.use(function(req, res, next) {
+  res.status(404)
+    .send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
+});
